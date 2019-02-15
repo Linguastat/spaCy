faae075d291a79b34f2536a451f1924589503cb0
Luis Aguilar <laguilar@voiseinc.com>
2019-02-15 14:26:48 -0800

    Making python3 specific updates to script.

--- setup.py
+++ setup.py
@@ -41,8 +41,7 @@ MOD_NAMES = [
     'spacy.matcher',
     'spacy.syntax.ner',
     'spacy.symbols',
-    'spacy.vectors',
-    'spacy.spacy_nlp'
+    'spacy.vectors'
 ]
 
 
--- spacy-update.sh
+++ spacy-update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Installing spaCy.
+# Installing spaCy to the root python3 conda environment.  In the future, 
+# I need to see if that can be done in a conda environment for production.
 
-# Verify python 3 is the main path installed version
-PYTHON=$(python -V 2>&1)
+# Verify python 3 is using the root conda version environment python3 alias
+PYTHON=$(python3 -V 2>&1)
 
 if [ -z "$CONDAHOME" ]
 then
@@ -24,29 +25,32 @@ else
 fi
 
 # store python version major and minor
-PYMAJOR=($(python -c "import sys; print(sys.version_info.major)"))
-PYMINOR=($(python -c "import sys; print(sys.version_info.minor)"))
+PYMAJOR=($(python3 -c "import sys; print(sys.version_info.major)"))
+PYMINOR=($(python3 -c "import sys; print(sys.version_info.minor)"))
 
 # move to spaCy and pull the latest 
-cd ~/spaCy
+cd $SPACYHOME
 # clone spaCy repo
-git fetch origin
-git checkout spacy2merge
-git pull origin spacy2merge 
+#git fetch origin
+#git checkout spacy2merge
+#git pull origin spacy2merge 
 # install requirements
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 # clean spaCy
-python setup.py clean
+python3 setup.py clean
 rm spacy/spacy_nlp.cpp
 # install spaCy
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 # install spaCy developed language models
 #python -m spacy download en
-python -m spacy download en_core_web_sm
-python -m spacy download en_core_web_md
+#python3 -m spacy download en_core_web_sm
+#python3 -m spacy download en_core_web_md
 # install our models
 
 # create the cpp files with cython
+sleep 2s
+echo -e "\n\n\nCython build of $SPACYHOME/spacy/spacy_nlp.cpp"
 cython --embed -o spacy/spacy_nlp.cpp spacy/spacy_nlp.py
 # build the binary wrapper
+echo -e "\n\n\ngcc build of spacy_nlp binary"
 gcc -v -Os -I ${CONDAHOME}/include/python${PYMAJOR}.${PYMINOR}m -L ${CONDAHOME}/lib -o ${SPACYHOME}/bin/spacy_nlp ${SPACYHOME}/spacy/spacy_nlp.cpp  -lpython${PYMAJOR}.${PYMINOR}m -lpthread -lm -lutil -ldl
